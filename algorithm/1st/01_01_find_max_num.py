399f0a5d37ab20434fc5819a1bd15db19f6dc590
SeungHee Han <hee11273@gmail.com>
2025-08-04 20:53:48 +0900

    최댓값 찾기
    최댓값 찾기

--- algorithm/1st/01_01_find_max_num.py
+++ algorithm/1st/01_01_find_max_num.py
@@ -2,9 +2,9 @@
 
 def find_max_num(array):
     for num in array:
-        is_max_num = True
+        is_max_num = True 
         for compare_num in array:
-            if num < compare_num:
+            if num < compare_num: #최대값
                 is_max_num=False
         if is_max_num:
             return num
@@ -20,4 +20,5 @@ def find_max_num(array):
 print("정답 = 6 / 현재 풀이 값 = ", find_max_num([3, 5, 6, 1, 2, 4]))
 print("정답 = 6 / 현재 풀이 값 = ", find_max_num([6, 6, 6]))
 print("정답 = 1888 / 현재 풀이 값 = ", find_max_num([6, 9, 2, 7, 1888]))
+
 
