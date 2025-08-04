102bc32e23cb1c145e826692fd1ae1896ba1a071
SeungHee Han <hee11273@gmail.com>
2025-08-04 20:51:41 +0900

    최빈값 찾기
    최빈값 찾기

--- algorithm/1st/01_01_find_max_num.py
+++ algorithm/1st/01_01_find_max_num.py
@@ -1,22 +1,23 @@
 #### sol1 ####
 
-# def find_max_num(array):
-#     for num in array:
-#         is_max_num = True
-#         for compare_num in array:
-#             if num < compare_num:
-#                 is_max_num=False
-#         if is_max_num:
-#             return num
+def find_max_num(array):
+    for num in array:
+        is_max_num = True
+        for compare_num in array:
+            if num < compare_num:
+                is_max_num=False
+        if is_max_num:
+            return num
 
 #### sol2 ####
-def find_max_num(array):
-    max_num = array[0]
-    for num in array:
-        if max_num < num:
-            max_num = num
-    return max_num
+# def find_max_num(array):
+#     max_num = array[0]
+#     for num in array:
+#         if max_num < num:
+#             max_num = num
+#     return max_num
 
 print("정답 = 6 / 현재 풀이 값 = ", find_max_num([3, 5, 6, 1, 2, 4]))
 print("정답 = 6 / 현재 풀이 값 = ", find_max_num([6, 6, 6]))
 print("정답 = 1888 / 현재 풀이 값 = ", find_max_num([6, 9, 2, 7, 1888]))
+
